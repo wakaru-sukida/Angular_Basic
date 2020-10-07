185e303bb1159f6700ed770513f6373d7d3f46e4
Weerasak Meesiang <weerasak.ingeni@gmail.com>
2020-10-07 10:02:38 +0700

    Last Commit some file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: '.app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BasicAngular';
+  title = 'BasicAngular 2020';
+  isShow = true;
+  languages = ['C#','VB','Mean Stack','Angular','Test'];
+  price = 2345.78;
+  myDate = new Date();
+
+  i:number = 0;
+  public addCount(){
+    this.i++;
+  }
+  showMessage(){
+    return this.title;
+  }
+
+  readData(value){
+    console.log(value);
+  }
 }
